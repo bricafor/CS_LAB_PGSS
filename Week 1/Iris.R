ffbc6b644593f64e884eb31651018fb59c30695b
bricafor <108365415+bricafor@users.noreply.github.com>
2022-06-28 17:11:17 -0400

    Update Iris.R

--- Week 1/Iris.R
+++ Week 1/Iris.R
@@ -1,13 +1,35 @@
+---
+title: "myFirstRMarkdown"
+author: "Prahlad Menon"
+date: '2022-06-28'
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## My first R Markdown
+
+```{r}
 library(readr)
 iris <- read_csv("iris.csv", col_names = F)
 colnames(iris) <- c('Sepal.Length', 'Sepal.Width', 'Petal.Length', 'Petal.Width', 'Class')
-
-sapplt(iris, class)
-
-iris$class <- as.factor(iris$class)
 sapply(iris, class)
+iris$Class <- as.factor(iris$Class)
+sapply(iris, class)
+# install.packages("dplyr")
+library(dplyr)
+iris %>% group_by(Class) %>% summarize( Min.Sepal.Length = min(Sepal.Length, na.rm=T),
+                                        Max.Sepal.Length = max(Sepal.Length, na.rm=T),
+                                        Mean.Sepal.Length = mean(Sepal.Length, na.rm=T),
+                                        SD.Sepal.Length = sd(Sepal.Length, na.rm=T))  %>% as.data.frame()
+```
 
-install.packages("dpylr")
-library(dyplr)
+```{r}
+boxplot(Sepal.Length ~ Class, data = iris)
+```
 
-View(iris)
+```{r}
+boxplot(Sepal.Width ~ Class, data = iris)
+```
