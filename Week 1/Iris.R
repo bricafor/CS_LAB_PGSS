69365dc79f0cfa08e30b19564eedb047a06d2b9f
bricafor <108365415+bricafor@users.noreply.github.com>
2022-06-28 17:12:40 -0400

    Update Iris.R

--- Week 1/Iris.R
+++ Week 1/Iris.R
@@ -10,4 +10,9 @@ sapply(iris, class)
 install.packages("dpylr")
 library(dyplr)
 
+iris %>% group_by(Class) %>% summarize( Min.Sepal.Length = min(Sepal.Length, na.rm=T),
+                                        Max.Sepal.Length = max(Sepal.Length, na.rm=T),
+                                        Mean.Sepal.Length = mean(Sepal.Length, na.rm=T),
+                                        SD.Sepal.Length = sd(Sepal.Length, na.rm=T))  %>% as.data.frame()
+
 View(iris)
